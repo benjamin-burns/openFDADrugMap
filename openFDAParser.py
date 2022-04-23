e18014b54901226ea43af7dca5157155ddb00edd
benjamin-burns <burns.benjamin01@gmail.com>
2022-04-23 01:14:45 -0400

    Implemented parsing methods
    Implemented parseFile and parseDrug, which currently build a JSON object. After further consideration, it makes more sense to output to a cvs file.

--- openFDAParser.py
+++ openFDAParser.py
@@ -1,78 +1,104 @@
 import json
-import csv
 
-
-
-with open("drug-label-0001-of-0010.json", "r") as input:
-    rawData = input.read()
-
-data = json.loads(rawData)
-
-entriesInFile = len(data["results"])
-
-for i in range(entriesInFile):
-    pass
-
-
-def parseDrug(drugData, out):
+def parseDrug(drugData):
     '''
-    Method that parses input json data of an individual drug given in {drugData}
-    and writes output to {out}.
+    Parses input JSON data of an individual drug given by {drugData}
+    and returns a condensed JSON representation.
+
+        Parameters:
+            drugData (JSON object)
+                    dictionary holding drug information from openFDA
+
+        Returns:
+            A JSON representation of drug name mapping data with formatting
+              * brand_name
+              * generic_name
+
+        Requires:
+            {drugData} formatting is consistent with openFDA formatting
+
+        Ensures:
+            {parseDrug} = [a JSON representation of a subset of the data contained
+            in {drugData}, with the formatting described in {Returns}]
     '''
-    # TODO
-    pass
+    try:
+        drugBrandName = drugData["openfda"]["brand_name"][0]
+    except KeyError:
+        print("Error: brand_name is not an attribute of the following data:\n" + str(drugData["openfda"]))
+        exit()
+
+    try:
+        drugGenericName = drugData["openfda"]["generic_name"][0]
+    except KeyError:
+        print("Error: generic_name is not an attribute of the following data:\n" + str(drugData["openfda"]))
+        exit()
+    
+
+    drugDataRep = {}
+    drugDataRep["brand_name"] = drugBrandName.lower()
+    drugDataRep["generic_name"] = drugGenericName.lower()
+
+    return drugDataRep
 
 
-def parseFile(inputFile, out):
+def parseFile(inputFile, drugMappings):
     '''
-    Parses {inputFile} and outputs drug name mappings to an output file using {out}.
+    Parses input JSON data given by {inputFile} and appends condensed JSON
+    data representation containing drug name mapping data for all drugs in
+    {inputFile} into {drugMappings}.
 
         Parameters:
             inputFile (string)
                     file path in openFDA input file
-            out (Writer)
-                    output stream to csv file
+            drugMappings (JSON object)
+                    dictionary to which to add drug name mapping data
 
         Updates:
-            out
+            drugMappings
 
         Requires:
-            {inputFile} is a valid file path to a readable json file in proper openFDA format,
-            {out} is open to a file to which the program can write, and
-            {out} already contains an appropriate csv header
+            {inputFile} is a valid file path to a readable JSON file in proper openFDA format
 
         Ensures:
-            The content of {out} is updated to contain all drug name mappings contained in
-                {inputFile}
+            The content of {drugMappings} is updated to contain all drug name mappings contained in
+            {inputFile}. If the input file cannot be opened, an error is thrown and program
+            execution stops
     '''
     # Open and read in data from {inputFile}
-    with open(inputFile, "r") as input:
-        rawData = input.read()
+    try:
+        with open(inputFile, "r") as input:
+            rawData = input.read()
+    except OSError:
+        print("Error: could not open file " + inputFile);
+        exit()
+
     data = json.loads(rawData)
     drugsInFile = len(data["results"])
 
-    for drugNum in drugsInFile:
+    for drugNum in range(drugsInFile):
         drug = data["results"][drugNum]
-        parseDrug(drug, out)
+        if not drug["openfda"] == {}:
+            drugMappings.update(parseDrug(drug))
 
 
 def main():
     '''
     Main method for openFDAParser.
-    Outputs a csv file with mappings between drug brand names and drug generic names.
-    No reformatting is performed on the input (i.e. the values in the mapping are exactly 
-        as they appear in the input)
+    Outputs a JSON file with mappings between drug brand names and drug generic names.
+    All input text is converted to lowercase. Otherwise, output mappings are as
+    exactly as they appear in input.
     '''
     FILE_NAMES_LIST = ["openFDAInput1.json"]
-    OUTPUT_FILE_PATH = "drugMapping.csv"
+    OUTPUT_FILE_PATH = "drugMapping.json"
 
-    outputFile = open(OUTPUT_FILE_PATH)
-    outputWriter = csv.writer(outputFile)
+    #outputFile = open(OUTPUT_FILE_PATH)
+    
+    drugMappings = {}
 
     for file in FILE_NAMES_LIST:
-        parseFile(file, outputWriter)
-    
+        parseFile(file, drugMappings)
 
+    print(drugMappings)
 
 if __name__=="__main__":
     main()
